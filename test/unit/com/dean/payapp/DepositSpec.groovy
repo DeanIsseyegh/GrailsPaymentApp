75b4bda2b57c2b445509ee957ac474af8dff6a84
DeanIsseyegh <issdea@gmail.com>
2015-06-09 09:12:01 +0100

    Fixed formatting

--- test/unit/com/dean/payapp/DepositSpec.groovy
+++ test/unit/com/dean/payapp/DepositSpec.groovy
@@ -10,25 +10,25 @@ import grails.validation.ValidationException
 @TestFor(Deposit)
 class DepositSpec extends Specification {
 
-        void "Deposit should not be able to save without a transaction and account"() {
+	void "Deposit should not be able to save without a transaction and account"() {
 		given:
 		def deposit = new Deposit()
-		
+
 		when:
 		deposit.save()
-		
+
 		then:
-		thrown ValidationException 
-    }
-	
+		thrown ValidationException
+	}
+
 	void "Deposit should be able to save with a transaction and account"() {
 		given:
-		def deposit = new Deposit(transaction: new Transaction(amount:50), 
-			account: new Account(accountName: "Bob", email: "bob@gmail.com"))
-		
+		def deposit = new Deposit(transaction: new Transaction(amount:50),
+		account: new Account(accountName: "Bob", email: "bob@gmail.com"))
+
 		when:
 		deposit.save()
-		
+
 		then:
 		notThrown ValidationException
 		deposit.validate() == true
